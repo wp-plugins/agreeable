ffe4b02ea859f2eb0cc37d29d3f127a581355cf9
buildcreate <buildcreate@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-02-12 19:02:56 +0000

    Fixed registration page glitch
    git-svn-id: https://plugins.svn.wordpress.org/agreeable/tags/0.1.2.1@856577 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ agreeable-options.php
@@ -0,0 +1,95 @@
+<?php 
+	    if($_POST['ag_hidden'] == 'Y') {
+			//Form data sent
+			 
+            $dbfail = $_POST['ag_fail'];
+          $dbfail = stripslashes($dbfail);
+            update_option('ag_fail', $dbfail);
+          
+          $dbtermm = $_POST['ag_termm'];
+         $dbtermm = stripslashes($dbtermm);
+            update_option('ag_termm', $dbtermm);
+            
+          $dburl = $_POST['ag_url'];
+          update_option('ag_url', $dburl);
+            
+          $dblogin = $_POST['ag_login'];      
+          update_option('ag_login', $dblogin);
+
+          $dbregister = $_POST['ag_register'];
+          update_option('ag_register', $dbregister);
+	
+			 ?> 
+		
+			<div class="updated"><p><strong><?php _e('Options saved.' ); ?></strong></p></div>  
+			<?php
+		} else {
+		//Normal page display
+		  $dbfail = get_option('ag_fail');
+		  $dbtermm = get_option('ag_termm');
+		  $dburl = get_option('ag_url');
+		  $dblogin = get_option('ag_login');
+		  $dbregister = get_option('ag_register');
+		}
+	?>
+	
+<style>
+
+.mes {
+color: gray;
+font-style: italic;
+font-size:. 9em;
+}
+#ag-form {
+width: 50%;
+}
+textarea {
+clear: right;
+float: right;
+}
+input[type="submit"] {
+margin-top: 2em;
+}
+#ag-form label {display: block; margin-bottom: .25em;}
+#ag-form .checkboxes label {display: inline;}
+
+#ag-form h3 {border-bottom: 1px solid #ccc; margin-bottom: .5em; padding-bottom: .5em; color: #369; text-shadow: 0 1px 1px #fff;}
+#ag-form input[type="text"] {min-width: 400px;}
+#feedback-form input[type="email"], #feedback-form textarea {width: 100%; display: block;}
+#feedback-form textarea {min-height: 100px;}
+</style>
+<div class="wrap">
+			
+			<div style="position: fixed; right: 10%; top: 13%; width: 25%; min-height: 200px; padding: 1.5em; background: #fafafa; border: 1px solid #ccc;">
+				<?php feedback_form(); ?>
+			</div>
+			
+			<?php echo "<h2>" . __( 'Agreeable Settings', 'ag_trdom' ) . "</h2>"; ?>
+			
+			<form id="ag-form" name="ag_form" method="post" action="<?php echo str_replace( '%7E', '~', $_SERVER['REQUEST_URI']); ?>">
+				<input type="hidden" name="ag_hidden" value="Y">
+				
+				<?php    echo "<h3>" . __( 'Settings', 'ag_trdom' ) . "</h3>"; ?>
+				
+				<p><label for="ag_fail"><?php _e("Failed to agree error message: " ); ?></label><input type="text" name="ag_fail" value="<?php echo $dbfail; ?>" size="20"></br><?php _e("<span class='mes'>This is what shows up if they don't check the box</span>" ); ?></p>
+				
+				<p><label for="ag_url"><?php _e("URL for Terms: " ); ?></label><input type="text" name="ag_url" value="<?php echo $dburl; ?>" size="20"></br><?php _e("<span class='mes'>This is the URL where the user can read your terms</span>" ); ?></p>
+				
+				<p><label for="ag_termm"><?php _e("Message: " ); ?></label><input type="text"  name="ag_termm" size="40" value="<?php echo $dbtermm; ?>"><br><?php _e("<span class='mes'>This is the text that goes right after the checkbox</span>" ); ?></p>
+				<br>
+
+				<div class="checkboxes">
+								<p>
+				<h3><?php _e("Where should it be displayed? " ); ?></h3>
+					<input type="checkbox" id="ag_login" name="ag_login" value="1" <?php if($dblogin == 1) {echo 'checked';} ?> /> <label for="ag_login">Login form</label><br>
+					<input type="checkbox" id="ag_register" name="ag_register" value="1" <?php if($dbregister == 1) {echo 'checked';} ?> /> <label for="ag_register">Registration form</label>
+					</p>
+				</div>
+			
+				<p class="submit">
+				<input type="submit" name="Submit" value="<?php _e('Update Options', 'ag_trdom' ) ?>" />
+				</p>
+			</form>
+
+		</div>
+	
---
+++ agreeable.php
@@ -0,0 +1,122 @@
+<?php
+/*
+Plugin Name: Agreeable
+Plugin URI: http://wordpress.org/extend/plugins/agreeable
+Description: Add a required "Agree to terms" checkbox to login and/or register forms.  Based on the I-Agree plugin by Michael Stursberg.
+Version: 0.1.2.1
+Author: buildcreate, thesturs
+Author URI: http://buildcreate.com
+*/
+
+function wp_authenticate_user_acc($user, $password) {
+	
+	$dblogin = get_option('ag_login');
+	$dbregister = get_option('ag_register');
+	
+	if($GLOBALS['pagenow'] == 'wp-login.php' && $dblogin == 1 || in_array('login', $body_class) && $dblogin == 1 || $GLOBALS['pagenow'] == 'wp-register.php' && $dbregister == 1 || in_array('register', $body_class) && $dbregister == 1 ) {
+	
+		 $dbfail = get_option('ag_fail');
+		  
+		  // See if the checkbox #login_accept was checked
+	    if ( isset( $_REQUEST['login_accept'] ) && $_REQUEST['login_accept'] == 'on' ) {
+	        // Checkbox on, allow login
+	        return $user;
+	    } else {
+	        // Did NOT check the box, do not allow login
+	        $error = new WP_Error();
+	        $error->add('did_not_accept', $dbfail);
+	        return $error;
+	    }
+    } else {
+	    return $user;
+    }
+}
+
+// As part of WP login form construction, call our function
+add_filter ( 'login_form', 'terms_accept' );
+add_filter ( 'register_form', 'terms_accept' );
+add_action('bp_before_registration_submit_buttons', 'terms_accept');
+
+function terms_accept(){
+	$dbtermm = get_option('ag_termm');
+	$dburl = get_option('ag_url');
+	$dblogin = get_option('ag_login');
+	$dbregister = get_option('ag_register');
+	global $post;
+	$body_class = get_body_class();
+	
+if($GLOBALS['pagenow'] == 'wp-login.php' && $dblogin == 1 || in_array('login', $body_class) && $dblogin == 1 || $GLOBALS['pagenow'] == 'wp-register.php' && $dbregister == 1 || in_array('register', $body_class) && $dbregister == 1 ) {
+
+    
+    	// Add an element to the login form, which must be checked
+    	echo '<div id="terms-accept"><label><input type="checkbox" name="login_accept" id="login_accept" />&nbsp;<a target="_BLANK" href="'.$dburl.'">'.$dbtermm.'</a></label></div><br>';
+    }
+}
+
+function agreeable_options() {  
+  add_options_page('agreeable', 'Agreeable', 'manage_options', 'terms-options', 'agoptions');
+}
+
+function agoptions() {		  
+	include('agreeable-options.php');
+}
+
+// Add to the admin menu
+add_action('admin_menu', 'agreeable_options');
+
+// Add it to the appropriate hooks
+add_filter('wp_authenticate_user', 'wp_authenticate_user_acc', 99999, 2);
+add_filter('registration_errors', 'wp_authenticate_user_acc', 99999, 2);
+add_filter('bp_signup_validate', 'wp_authenticate_user_acc', 9999, 2);
+
+
+/* Plugin feedback form */
+
+function feedback_form() {
+	
+	if(!$_POST['feedback_email'] && !$_POST['feedback_content']) {
+	
+	$output = '<h3>We want your feedback.</h3>
+				<p><em>Have a feature idea, feedback, or question about the plugin?<br>We want to know- send it on over!</em></p>
+				<form id="feedback-form" name="feedback_form" method="post" action="'.str_replace( '%7E', '~', $_SERVER['REQUEST_URI']).'">
+				<label for="feedback_email">Your email</label>
+					<input type="email" name="feedback_email" placeholder="your@email.com" /><br>
+				<label for="feedback_content">Message</label>
+					<textarea name="feedback_content" placeholder="Type your feedback / feature request here!"></textarea><br>
+					<input type="submit" class="button-primary button-large button" style="margin-top: 1em;" value="Send it!" />			
+				</form>';
+	$output .= '<div style="padding: 1em; background: #eee; color: #333; margin-top: 2em;">
+					<h3 style="color: #369;">Buy me a cup of joe?</h3>
+					<p>
+						<em>Feeling generous?  Because I sure wouldn\'t turn down a hot cup of coffee...</em>
+					</p>
+					<p>
+						<form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
+							<input type="hidden" name="cmd" value="_s-xclick">
+							<input type="hidden" name="hosted_button_id" value="LCNWR8KVE3UVL">
+							<input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_paynow_SM.gif" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!">
+							<img alt="" border="0" src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif" width="1" height="1">
+						</form>
+					</p></div>';
+	} else {
+		$output = '<h3>Thank you for your feedback!</h3>';
+	}
+	
+	echo $output;
+}
+
+function send_feedback() {
+	if(isset($_POST['feedback_email']) && isset($_POST['feedback_content'])) {
+		
+		$to = 'ian@buildcreate.com';
+		$subject = 'New plugin feedback';
+		$message = $_POST['feedback_content'];
+		$headers = 'From: <'.$_POST['feedback_email'].'>' . "\r\n";
+		
+		
+		wp_mail( $to, $subject, $message, $headers, '' );
+		
+	}
+}
+
+add_action( 'plugins_loaded', 'send_feedback');

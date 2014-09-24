54f1e1d332f04d4e29ae58a538651962bc934cc0
buildcreate <buildcreate@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-09-24 16:27:26 +0000

    Fixed editor issue
    git-svn-id: https://plugins.svn.wordpress.org/agreeable/tags/1.3.5@996181 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ js/agreeable.js
@@ -0,0 +1,14 @@
+jQuery(document).ready(function($) {
+
+	$(document).on("DOMSubtreeModified", function(){
+		$('.ag-open-popup-link').magnificPopup({
+		  type:'inline',
+		  midClick: true,
+		});
+	});
+	
+	if($('.woocommerce .login')) {
+		$(".woocommerce>#agreeable_login_field").insertBefore(".woocommerce .login #rememberme");
+	}
+			
+});
